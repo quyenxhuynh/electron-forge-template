12ad9de2111454353c8f048b17f674284e8a26f5
Quyen Huynh <qhuynh7@gmu.edu>
2025-12-01 19:46:23 -0800

    Base app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,1 +1,11 @@
-console.log('Renderer process started');
+import { createRoot } from "react-dom/client";
+
+export default function App() {
+  return <div>App</div>;
+}
+
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
